b4af4e9687d8381bb29b68c5e559c77f5808ace6
Davide De Pretto <davide.depretto@icloud.com>
2021-04-06 21:38:09 +0200

    Add day 9

---
+++ day_0009/main.py
@@ -0,0 +1,47 @@
+def get_sum_bigger_non_adjacent(l):
+    '''
+    >>> get_sum_bigger_non_adjacent([])
+
+    >>> get_sum_bigger_non_adjacent([100])
+
+    >>> get_sum_bigger_non_adjacent([100, 101])
+
+    >>> get_sum_bigger_non_adjacent([5, 7, 6])
+    11
+    >>> get_sum_bigger_non_adjacent([5, 1, 1, 5])
+    10
+    >>> get_sum_bigger_non_adjacent([90, 70, 100, 101])
+    191
+    >>> get_sum_bigger_non_adjacent([101, 90, 70, 100])
+    201
+    >>> get_sum_bigger_non_adjacent([90, 101, 70, 100])
+    201
+    >>> get_sum_bigger_non_adjacent([2, 4, 6, 2, 5])
+    11
+    '''
+
+    if len(l) <= 2:
+        return None
+
+    if len(l) == 3:
+        return l[0] + l[2]
+
+    max_value_1 = float('-inf')
+    max_index_1 = None
+    for i, element in enumerate(l):
+        if element > max_value_1:
+            max_value_1 = element
+            max_index_1 = i
+
+    max_value_2 = float('-inf')
+    for i, element in enumerate(l):
+        if i not in [max_index_1 - 1, max_index_1, max_index_1 + 1]:
+            if element > max_value_2:
+                max_value_2 = element
+
+    return max_value_1 + max_value_2
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
